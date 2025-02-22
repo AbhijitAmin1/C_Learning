561cac6907100aae112600ebb99f2982bf7a448a
AbhijitAmin1 <abhijitamin121@gmail.com>
2025-02-22 20:26:17 +0530

    My First C program

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,13 @@
 
 int main(){
 
-    printf("I like pizza!\n");
+//This is a comment
+/*This 
+is
+ a
+  multiline 
+  comment */
+    printf("I like pizza!\n");  
     printf("Its really good!\n");
     return 0;
 }
